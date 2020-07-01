5db904e90e87670312cdfee730d19b8f480bee04
mbourisly <49692793+mbourisly@users.noreply.github.com>
2020-07-01 17:53:41 +0300

    done

---
+++ CW 6/app/src/main/java/com/example/classwork/movie.java
@@ -0,0 +1,10 @@
+package com.example.classwork;
+
+public class movie {
+
+    private String title ;
+    private String mainActor ;
+    private double movieRate ;
+    private int pgRate ;
+    private String genre ;
+}
